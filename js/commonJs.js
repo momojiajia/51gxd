2f6c9d3f759f0af1c1157e53c5ca2b39d994bb4f
Grace <910638634@qq.com>
2017-08-26 10:27:34 +0800

    Initial commit

---
+++ js/commonJs.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Grace on 2017/2/11 0011.
+ */
+
+//个人滑入显示
+/*var userHover = function(){
+    $('#user-name').hover(function(){
+        $('#user').css('display','block');
+    },function(){
+        $('#user').css('display','none');
+    })
+};*/
+
+//userHover();
